18535e630f17b79b77c7d11e76f50a3f25c91519
yoshihiko_ikeda <surf.marine.suki.suki@gmail.com>
2015-12-16 02:50:32 +0000

    implementathon of the validation

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,6 @@
 class Message < ActiveRecord::Base
-    #　名前は必須入力かつ２０文字以上
-    validates :name , length:{ maximum: 20 } , precence: true
-    # 内容は必須入力かつ2文字以上30文字以内
-    validates :body , length:{ minimum: 2 , maximum :30 } , precence:true
+    # 名前は必須入力かつ20文字以内
+  validates :name , length: { maximum: 20 } , presence: true
+  # 内容は必須入力かつ2文字以上30文字以下
+  validates :body , length: { minimum: 2, maximum: 30 } , presence: true
 end
